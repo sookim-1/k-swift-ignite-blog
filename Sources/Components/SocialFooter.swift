057a3035a88e0bef83b124d2bd85fde4932fdcf0
sookim-1 <scstnghks@gmail.com>
2024-12-17 13:10:21 +0900

    chore: medium 블로그 링크 풋터 추가

--- Sources/Components/SocialFooter.swift
+++ Sources/Components/SocialFooter.swift
@@ -11,12 +11,14 @@ import Ignite
 public struct SocialFooter: Component {
     let icons = [
         Image(systemName: "github"),
-        Image(systemName: "linkedin")
+        Image(systemName: "linkedin"),
+        Image(systemName: "medium")
     ]
 
     let urlStrings = [
         "https://github.com/sookim-1",
-        "https://www.linkedin.com/in/sookim1"
+        "https://www.linkedin.com/in/sookim1",
+        "https://sookim-1.medium.com"
     ]
 
     public func body(context: PublishingContext) -> [any PageElement] {
